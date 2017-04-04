b5c1ef261041e63f77a00cc5a61f135bb1a70558
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-04-04 12:58:05 -0300

    Added a few functions to MetaEvent

--- src/LuaMidi/MetaEvent.lua
+++ src/LuaMidi/MetaEvent.lua
@@ -1,13 +1,16 @@
+local Constants = require('LuaMidi.Constants')
 local Util = require('LuaMidi.Util')
 
 local MetaEvent = {}
 
 function MetaEvent.new(fields)
    local self = {
-      type = 'meta',
-      data = Util.num_to_var_length(0x00),
+      type = 'meta'
    }
-   self.data = -- TODO
+   local data = Util.num_to_var_length(0x00)
+   data[#data+1] = Constants.META_EVENT_ID
+   data[#data+1] = fields.data
+   self.data = data
    return setmetatable(self, { __index = MetaEvent })
 end
 
