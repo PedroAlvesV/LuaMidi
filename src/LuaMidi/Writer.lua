378722688f9cf1e5d7e22fb9706b4be0eb4cced7
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-04-05 17:48:21 -0300

    Added remaining lines to Writer constructor

--- src/LuaMidi/Writer.lua
+++ src/LuaMidi/Writer.lua
@@ -11,9 +11,16 @@ function Writer.new(tracks)
    if #tracks > 1 then
       track_type = Constants.HEADER_CHUNK_FORMAT1
    end
-   local num_tracks = Util.number_to_bytes(#tracks, 2)
-   self.data = -- TODO
-   tracks -- TODO
+   local chunk_data = Util.table_concat(track_type, Util.number_to_bytes(#tracks, 2))
+   chunk_data = Util.table_concat(chunk_data, Constants.HEADER_CHUNK_DIVISION)
+   self.data[#self.data+1] = Chunk.new({
+      type = Constants.HEADER_CHUNK_TYPE,
+      data = chunk_data,
+   })
+   for i, track in ipairs(tracks) do
+      track:add_event(MetaEvent.new({data = Constants.META_END_OF_TRACK_ID}))
+      self.data[#self.data+1] = track
+   end
    return setmetatable(self, { __index = Writer })
 end
 
