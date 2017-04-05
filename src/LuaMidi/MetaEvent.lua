2ef938b4979aa8f71f735cff8d395521d0eaa0d6
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-04-05 17:45:11 -0300

    Added remaining lines to ProgramChangeEvent
    Revised MetaEvent to match it, because they are similar.

--- src/LuaMidi/MetaEvent.lua
+++ src/LuaMidi/MetaEvent.lua
@@ -4,13 +4,13 @@ local Util = require('LuaMidi.Util')
 local MetaEvent = {}
 
 function MetaEvent.new(fields)
-   local self = {
-      type = 'meta'
-   }
    local data = Util.num_to_var_length(0x00)
    data[#data+1] = Constants.META_EVENT_ID
    data[#data+1] = fields.data
-   self.data = data
+   local self = {
+      type = 'meta',
+		data = data,
+   }
    return setmetatable(self, { __index = MetaEvent })
 end
 
--- src/LuaMidi/ProgramChangeEvent.lua
+++ src/LuaMidi/ProgramChangeEvent.lua
@@ -1,11 +1,16 @@
+local Constants = require('LuaMidi.Constants')
+local Util = require('LuaMidi.Util')
+
 local ProgramChangeEvent = {}
 
 function ProgramChangeEvent.new(fields)
+   local data = Util.num_to_var_length(0x00)
+   data[#data+1] = Constants.PROGRAM_CHANGE_STATUS
+   data[#data+1] = fields.data
    local self = {
       type = 'program',
-      data = Util.num_to_var_length(0x00),
+		data = data,
    }
-   self.data = -- TODO
    return setmetatable(self, { __index = ProgramChangeEvent })
 end
 
