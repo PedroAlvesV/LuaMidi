ae8a98330da5d80fadea73665a61290337210015
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-04-13 17:06:42 -0300

    Fixed misbinding on Chunk class module

--- src/LuaMidi/Chunk.lua
+++ src/LuaMidi/Chunk.lua
@@ -4,7 +4,7 @@ function Chunk.new(fields)
    local self = {
       type = fields.type,
       data = fields.data,
-      size = {0, 0, 0, fields.data.length},
+      size = {0, 0, 0, #fields.data},
    }
    return setmetatable(self, { __index = Chunk })
 end
