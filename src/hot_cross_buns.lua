a53cd3991ff4a49f8f9161fbccc302dcc38ef235
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-04-13 15:48:13 -0300

    Added example

---
+++ src/hot_cross_buns.lua
@@ -0,0 +1,24 @@
+local LuaMidi = require ('LuaMidi')
+local Track = LuaMidi.Track
+local NoteEvent = LuaMidi.NoteEvent
+local Writer = LuaMidi.Writer
+
+local track = Track.new()
+
+track:add_event(
+   {
+      NoteEvent.new({pitch = {'E4', 'D4'}, duration = '4'}),
+      NoteEvent.new({pitch = {'C4'}, duration = '2'}),
+      NoteEvent.new({pitch = {'E4', 'D4'}, duration = '4'}),
+      NoteEvent.new({pitch = {'C4'}, duration = '2'}),
+      NoteEvent.new({pitch = {'C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'}, duration = '8'}),
+      NoteEvent.new({pitch = {'E4', 'D4'}, duration = '4'}),
+      NoteEvent.new({pitch = {'C4'}, duration = '2'}),
+   },
+   function(index, event)
+      return {sequencial = true}
+   end
+)
+
+local writer = Writer.new({track})
+writer:save_MIDI('hot-cross-buns')
