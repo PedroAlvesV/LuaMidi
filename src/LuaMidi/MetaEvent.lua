aed017af1e51370b26c98f6d88c383178b876668
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-04-13 20:27:02 -0300

    Fixed small concat issue on MetaEvent constructor

--- src/LuaMidi/MetaEvent.lua
+++ src/LuaMidi/MetaEvent.lua
@@ -5,11 +5,11 @@ local MetaEvent = {}
 
 function MetaEvent.new(fields)
    local data = Util.num_to_var_length(0x00)
-   data[#data+1] = Constants.META_EVENT_ID
-   data[#data+1] = fields.data
+   data = Util.table_concat(data, Constants.META_EVENT_ID)
+   data = Util.table_concat(data, fields.data)
    local self = {
       type = 'meta',
-		data = data,
+      data = data,
    }
    return setmetatable(self, { __index = MetaEvent })
 end
