3a41ffb6449d2d4802afb8a0ceb0850a7b925b03
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-04-14 03:22:50 -0300

    Fixed var type o MetaEvent

--- src/LuaMidi/MetaEvent.lua
+++ src/LuaMidi/MetaEvent.lua
@@ -5,7 +5,7 @@ local MetaEvent = {}
 
 function MetaEvent.new(fields)
    local data = Util.num_to_var_length(0x00)
-   data = Util.table_concat(data, Constants.META_EVENT_ID)
+   data = Util.table_concat(data, {Constants.META_EVENT_ID})
    data = Util.table_concat(data, fields.data)
    local self = {
       type = 'meta',
