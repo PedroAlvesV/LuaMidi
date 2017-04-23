6f3fa649250d2bc0376b33362dcbe8e7564f4c39
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-04-22 21:09:44 -0300

    Renamed functions on Stairway to Heaven example

--- src/stairway_to_heaven.lua
+++ src/stairway_to_heaven.lua
@@ -5,31 +5,31 @@ local Writer = LuaMidi.Writer
 
 local track = Track.new()
 
-local function foo(pitch)
+local function note(pitch)
    return NoteEvent.new({pitch = {pitch}})
 end
-local function bar(array)
+local function chord(array)
    return NoteEvent.new({pitch = array})
 end
 
-local A4 = foo('A4')
-local C5 = foo('C5')
-local E5 = foo('E5')
-local A5 = foo('A5')
-local Ab4_B5 = bar({'G#4', 'B5'})
-local B5 = foo('B5')
-local G4_C6 = bar({'G4', 'C6'})
-local C6 = foo('C6')
-local Gb4_Gb5 = bar({'F#4', 'F#5'})
-local D5 = foo('D5')
-local Gb5 = foo('F#5')
-local F4_E5 = bar({'F4', 'E5'})
+local A4 = note('A4')
+local C5 = note('C5')
+local E5 = note('E5')
+local A5 = note('A5')
+local Ab4_B5 = chord({'G#4', 'B5'})
+local B5 = note('B5')
+local G4_C6 = chord({'G4', 'C6'})
+local C6 = note('C6')
+local Gb4_Gb5 = chord({'F#4', 'F#5'})
+local D5 = note('D5')
+local Gb5 = note('F#5')
+local F4_E5 = chord({'F4', 'E5'})
 local long_C5 = NoteEvent.new({pitch = {'C5'}, duration = '2'})
 
 local Am = {'A3', 'E4', 'A4', 'C5'}
 
-local chord_GB = bar({'B3', 'D4', 'G4', 'B4'})
-local chord_Am = bar(Am)
+local chord_GB = chord({'B3', 'D4', 'G4', 'B4'})
+local chord_Am = chord(Am)
 local long_chord_Am = NoteEvent.new({pitch = Am, duration = '2'})
 
 track:add_event({
