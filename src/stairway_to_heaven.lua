92c229df4c00782d82247f741cece057b724c934
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-04-23 00:39:45 -0300

    Improved Stairway to Heaven example

--- src/stairway_to_heaven.lua
+++ src/stairway_to_heaven.lua
@@ -5,37 +5,37 @@ local Writer = LuaMidi.Writer
 
 local track = Track.new()
 
-local function note(pitch)
-   return NoteEvent.new({pitch = {pitch}})
+local function note(pitch, duration)
+   return NoteEvent.new({pitch = {pitch}, duration = tostring(duration)})
 end
-local function chord(array)
-   return NoteEvent.new({pitch = array})
+local function chord(array, duration)
+   return NoteEvent.new({pitch = array, duration = tostring(duration)})
 end
 
+local A3 = note('A3')
+local C4 = note('C4')
+local E4 = note('E4')
 local A4 = note('A4')
+local Ab3_B4 = chord({'G#3', 'B4'})
+local B4 = note('B4')
+local G3_C5 = chord({'G3', 'C5'})
 local C5 = note('C5')
-local E5 = note('E5')
-local A5 = note('A5')
-local Ab4_B5 = chord({'G#4', 'B5'})
-local B5 = note('B5')
-local G4_C6 = chord({'G4', 'C6'})
-local C6 = note('C6')
-local Gb4_Gb5 = chord({'F#4', 'F#5'})
-local D5 = note('D5')
-local Gb5 = note('F#5')
-local F4_E5 = chord({'F4', 'E5'})
-local long_C5 = NoteEvent.new({pitch = {'C5'}, duration = '2'})
+local Gb3_Gb4 = chord({'F#3', 'F#4'})
+local D4 = note('D4')
+local Gb4 = note('F#4')
+local F3_E4 = chord({'F3', 'E4'})
+local long_C4 = note('C4', 2)
 
-local Am = {'A3', 'E4', 'A4', 'C5'}
+local Am = {'A2', 'E3', 'A3', 'C4'}
 
-local chord_GB = chord({'B3', 'D4', 'G4', 'B4'})
+local chord_GB = chord({'B2', 'D3', 'G3', 'B3'})
 local chord_Am = chord(Am)
-local long_chord_Am = NoteEvent.new({pitch = Am, duration = '2'})
+local long_chord_Am = chord(Am, 1)
 
 track:add_event({
-   A4, C5, E5, A5, Ab4_B5, E5, C5, B5,
-   G4_C6, E5, C5, C6, Gb4_Gb5, D5, A4,
-   Gb5, F4_E5, C5, A4, long_C5, E5, C5, A4,
+   A3, C4, E4, A4, Ab3_B4, E4, C4, B4,
+   G3_C5, E4, C4, C5, Gb3_Gb4, D4, A3,
+   Gb4, F3_E4, C4, A3, long_C4, E4, C4, A3,
    chord_GB, chord_Am, long_chord_Am
 })
 
