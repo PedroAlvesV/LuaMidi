824ef947dfda0b080570f67947f4a0c18ca4e2dd
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-04-27 00:18:32 -0300

    Fixed ProgramChangeEvent

--- src/LuaMidi/ProgramChangeEvent.lua
+++ src/LuaMidi/ProgramChangeEvent.lua
@@ -23,7 +23,7 @@ local ProgramChangeEvent = {}
 function ProgramChangeEvent.new(fields)
    local data = Util.num_to_var_length(0x00)
    data[#data+1] = Constants.PROGRAM_CHANGE_STATUS
-   data[#data+1] = fields.data
+   data[#data+1] = fields.instrument
    local self = {
       type = 'program',
 		data = data,
