7ee14ead3f1fe0f8b72872b1a6065db464b5a469
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-05-17 04:36:27 -0300

    Minor changes to automatized test module

--- src/auto-test.lua
+++ src/auto-test.lua
@@ -5,9 +5,13 @@ local filenames = {
    'smoke_on_the_water',
 }
 
+local show_log = not (arg[1] == '--quiet' or arg[1] == '-q')
+
 local run
 for _, name in ipairs(filenames) do
-   print("Executing", name..".lua")
+   if show_log then
+      print("Executing", name..".lua")
+   end
    run = os.execute('lua '..name..'.lua')
    if not run then break end
 end
@@ -15,18 +19,24 @@ end
 if run then
    local LuaMidi = require('LuaMidi')
    for _, name in ipairs(filenames) do
-      print()
-      print(name..".mid")
+      if show_log then
+         print("\n"..name..".mid")
+      end
       local tracks = LuaMidi.get_MIDI_tracks('midi files/'..name..'.mid')
       for i, track in ipairs(tracks) do
-         print("Track #"..i)
-         print("\tName:   ",track:get_name())
-         print("\tCopyright:",track:get_copyright())
-         print("\tInstrument:",track:get_instrument_name())
-         print("\tLyric:   ",track:get_lyric())
-         print("\tText:   ",track:get_text())
-         print("\tMarker:   ",track:get_marker())
-         print("\tCue Point:",track:get_cue_point())
+         if show_log then
+            print("Track #"..i)
+            print("\tName:   ",track:get_name())
+            print("\tCopyright:",track:get_copyright())
+            print("\tInstrument:",track:get_instrument_name())
+            print("\tLyric:   ",track:get_lyric())
+            print("\tText:   ",track:get_text())
+            print("\tMarker:   ",track:get_marker())
+            print("\tCue Point:",track:get_cue_point())
+            print()
+         end
       end
    end
 end
+
+print("No errors, not stuck.")
