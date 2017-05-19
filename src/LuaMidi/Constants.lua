5349cb92295dbd9ac23fc741a0a21f3e9cd1b8b6
Pedro Alves Valentim <pedroalvesvalentim@gmail.com>
2017-05-19 04:32:26 -0300

    Added METADATA_TYPES constants table

--- src/LuaMidi/Constants.lua
+++ src/LuaMidi/Constants.lua
@@ -1,33 +1,35 @@
 -------------------------------------------------
--- MIDI Constants
+-- LuaMidi Constants
 -- 
 -- @classmod Constants
 -- @author Pedro Alves
 -- @license MIT
 -- @see LuaMidi
 -------------------------------------------------
+
 local Constants = {
-    HEADER_CHUNK_TYPE      = {0x4d, 0x54, 0x68, 0x64}, -- Mthd
-    HEADER_CHUNK_LENGTH    = {0x00, 0x00, 0x00, 0x06}, -- Header size for SMF
-    HEADER_CHUNK_FORMAT0   = {0x00, 0x00}, -- Midi Type 0 id
-    HEADER_CHUNK_FORMAT1   = {0x00, 0x01}, -- Midi Type 1 id
-    HEADER_CHUNK_DIVISION  = {0x00, 0x80}, -- Defaults to 128 ticks per beat
-    TRACK_CHUNK_TYPE       = {0x4d, 0x54, 0x72, 0x6b}, -- MTrk,
-    META_EVENT_ID          = 0xFF,
-    META_TEXT_ID           = 0x01,
-    META_COPYRIGHT_ID      = 0x02,
-    META_TRACK_NAME_ID     = 0x03,
-    META_INSTRUMENT_NAME_ID= 0x04,
-    META_LYRIC_ID          = 0x05,
-    META_MARKER_ID         = 0x06,
-    META_CUE_POINT         = 0x07,
-    META_TEMPO_ID          = 0x51,
-    META_SMTPE_OFFSET      = 0x54,
-    META_TIME_SIGNATURE_ID = 0x58,
-    META_KEY_SIGNATURE_ID  = 0x59,
-    META_END_OF_TRACK_ID   = {0x2F, 0x00},
-    PROGRAM_CHANGE_STATUS  = 0xC0, -- includes channel number (0)
-    NOTES                  = {},
+   HEADER_CHUNK_TYPE      = {0x4D, 0x54, 0x68, 0x64}, -- Mthd
+   HEADER_CHUNK_LENGTH    = {0x00, 0x00, 0x00, 0x06}, -- Header size for SMF
+   HEADER_CHUNK_FORMAT0   = {0x00, 0x00}, -- Midi Type 0 id
+   HEADER_CHUNK_FORMAT1   = {0x00, 0x01}, -- Midi Type 1 id
+   HEADER_CHUNK_DIVISION  = {0x00, 0x80}, -- Defaults to 128 ticks per beat
+   TRACK_CHUNK_TYPE       = {0x4D, 0x54, 0x72, 0x6B}, -- MTrk,
+   META_EVENT_ID          = 0xFF,
+   META_TEXT_ID           = 0x01,
+   META_COPYRIGHT_ID      = 0x02,
+   META_TRACK_NAME_ID     = 0x03,
+   META_INSTRUMENT_NAME_ID= 0x04,
+   META_LYRIC_ID          = 0x05,
+   META_MARKER_ID         = 0x06,
+   META_CUE_POINT         = 0x07,
+   META_TEMPO_ID          = 0x51,
+   META_TIME_SIGNATURE_ID = 0x58,
+   META_KEY_SIGNATURE_ID  = 0x59,
+   META_END_OF_TRACK_ID   = {0x2F, 0x00},
+   PROGRAM_CHANGE_STATUS  = 0xC0,
+   NOTES                  = {},
+   METADATA_TYPES = {"Text","Copyright","Name","Instrument","Lyric","Marker","Cue Point",
+      [81] = "Tempo",[88] = "Time Signature",[89] = "Key Signature"}
 }
 
 local table_notes = {
